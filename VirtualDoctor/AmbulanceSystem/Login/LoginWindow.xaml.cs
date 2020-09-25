a1a4c8ed6f529322865d26c25e54bf5ac92b67a1
Marija Novakovic <marija.vanja.novakovic@gmail.com>
2020-09-25 12:32:15 +0200

    Add themes and images

---
+++ VirtualDoctor/AmbulanceSystem/Login/LoginWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace AmbulanceSystem.Login
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
